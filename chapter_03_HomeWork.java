c8a5b05beb5c22b4268eff5fbad392cb21b9d2b7
Vladislav Kolotilov <kolotilov98@mail.ru>
2024-03-25 15:15:45 +0700

    #03.3 SQL запросы: Практика и ДЗ

---
+++ chapter_03_HomeWork.java
@@ -0,0 +1,25 @@
+public class chapter_03_HomeWork {
+    //#03.3 SQL запросы: Практика и ДЗ
+
+    //Для самообучающихся студентов и студентов курсов
+
+    //1. Откройте демо-базу.
+    //https://sqliteonline.com
+
+    //2. Покажите все записи с Id = 5, 6, 7 и отсортируйте результат по Hint в обратном порядке.
+    //SELECT * FROM demo
+    //WHERE id BETWEEN 5 and 7
+    //ORDER BY hint DESC;
+
+    //3. Проверьте, есть ли значения NULL хотя бы в одном поле базы данных.
+    //SELECT * FROM demo
+    //WHERE (name ISNULL or hint ISNULL)
+
+
+    //4. Добавьте в базу новую запись.
+    //INSERT INTO demo (name,hint)
+    //VALUES ('KolotilovVS','Lerning SQL')
+
+
+    //https://imgur.com/a/NX1k3B6
+}
