540f28ae63f0f23ec3d1fe71036dd6cbb0cc7355
Vladislav Kolotilov <kolotilov98@mail.ru>
2024-03-25 19:33:33 +0700

    #05.3 Вложенные запросы SQL: Практика и ДЗ

---
+++ chapter_05_HomeWork.java
@@ -0,0 +1,9 @@
+public class chapter_05_HomeWork {
+    //#05.3 Вложенные запросы SQL: Практика и ДЗ
+
+    //Для самообучающихся студентов и студентов курсов.
+    //Используйте эмулятор.
+    //https://www.w3schools.com/sql/trysql.asp?filename=trysql_asc
+    //Для последнего заказа покажите customer's name, product name и employee's name.
+    //Используйте подзапросы.
+}
